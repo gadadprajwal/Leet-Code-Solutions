c61610156f04ce67270fa5ab58b5966fe12252a3
Prajwal Gadad <31756844+gadadprajwal@users.noreply.github.com>
2018-08-03 16:36:21 -0400

    Create #322-Coin Change.py

---
+++ #322-Coin Change.py
@@ -0,0 +1,25 @@
+import math
+class Solution:
+    def coinChange(self, coins, amount):
+        # Table for storing previous results
+        dp = []
+        # Append the table initially with infinity
+        for i in range(amount+1):
+            dp.append(math.inf)
+        # Initialize the first index with 0 as it is the base case for our problem
+        dp[0] = 0
+        
+        # Find the minimum number of coins that could be used to get to the amount remaining and add that to your dp table
+        for i in range(1,amount+1):
+            min_ = math.inf
+            for j in range(len(coins)):
+                if i-coins[j] >= 0:
+                    if min_ > dp[i-coins[j]]:
+                        min_ = dp[i-coins[j]]
+            if min_ != math.inf:
+                dp[i] = min_+1
+        # If no combination is possible then the value is remained as infinity as it does not change and we return -1
+        if dp[amount] == math.inf:
+            return -1
+        else:
+            return dp[amount]
